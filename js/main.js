cdbcc3b39db0e4b8c421d615966cc974632e01e5
jibanez74 <jibanez.romany74@gmail.com>
2019-03-19 11:40:34 -0400

    added form attrs for saimple form submition

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 $(document).ready(() => {
-  //  ajaxForm();
-
   for (let i = 1; i < 12; i++) {
     if (i < 2) {
       $('#slides').append(`
@@ -43,21 +41,3 @@ $(document).ready(() => {
     }
   });
 });
-
-// function ajaxForm() {
-//   $('#myForm').submit(e => {
-//     e.preventDefault();
-
-//     const $form = $(this);
-//     $.post($form.attr('action'), $form.serialize()).then(() => {
-//       $('#myForm').fadeOut('slow');
-//       $('#success-card').fadeIn('slow');
-//       $('#name, email, phone, msg').val('');
-
-//       setTimeout(() => {
-//         $('#success-card').fadeOut('slow');
-//         $('#myForm').fadeIn('slow');
-//       }, 6000);
-//     });
-//   });
-// }
